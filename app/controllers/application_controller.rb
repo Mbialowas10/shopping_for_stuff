e2b59aaa17a7e70a9694391045dba0c77f6b18b1
Michael Bialowas <mbialowas@rrc.ca>
2022-10-27 09:58:45 -0500

    primative shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session, :increment_visit_count
+  helper_method :visit_count
+
+  private
+
+  def initialize_session
+    # this will initialize the visit count to zero for new users.
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    # this will increment the user's visit count in session
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    # this will return the vist count from the session
+    session[:visit_count]
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # started with lines 5-7 then refactored to move code into application_controller
+    # session[:visit_count] ||= 0
+    # session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 
   def show
