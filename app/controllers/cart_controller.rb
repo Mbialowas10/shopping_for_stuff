a21be9e1ff791be1f430dafd544fbe930f95e780
mbialowas10 <mbialowas@rrc.ca>
2023-10-25 13:43:17 -0500

    remove link (-) not deleting a product

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,26 @@
+class CartController < ApplicationController
+  # POST /cart
+  # Data sent as form data (params)
+  def create
+    # Adds params[:id] to cart
+    # the logger will show in the rails server.. super useful for debugging!!
+    logger.debug("Adding #{params[:id]} to cart.")
+    id = params[:id].to_i
+    # add product id to shopping cart if it doesn't already exist
+    session[:shopping_cart] << id unless session[:shopping_cart].include?(id) # pushes id onto the end of the array
+    product = Product.find(id)
+    flash[:notice] = "➕ #{product.name} added to cart."
+    redirect_to root_path
+  end
+
+  # DELETE /cart/:id
+  def destroy
+    # removes params[:id] from cart
+    # removes params[:id] from cart
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    product = Product.find(id)
+    flash[:notice] = "➖ #{product.name} removed from cart." # should be removed to line below
+    redirect_to root_path
+  end
+end
