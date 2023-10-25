443abdacdf0e8491ea0e164376a171948edf8ea5
mbialowas10 <mbialowas@rrc.ca>
2023-10-25 11:44:04 -0500

    customized application controller for cart actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :initialize_session, :increment_visit_count
-  helper_method :visit_count
+  before_action :initialize_session
+  helper_method :cart
 
   private
 
   def initialize_session
     # this will initialize the visit count to zero for new users.
     # default value when the thing on the left hand side is UNSET, when nil set to 0
-    session[:visit_count] ||= 0
+    session[:shopping_cart] ||= [] # empty array of product IDs
   end
 
-  def increment_visit_count
-    # this will increment the user's visit count in session.
-    session[:visit_count] += 1
-  end
-
-  def visit_count
-    # this will return the visit count from session.
-    session[:visit_count]
+  def cart
+    Product.find(session[:shopping_cart])
   end
 end
