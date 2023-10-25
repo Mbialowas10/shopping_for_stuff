3bca2c421ace3952a98a6b83ae414cf3cd8f3ddf
mbialowas10 <mbialowas@rrc.ca>
2023-10-25 11:39:57 -0500

    Session Implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session, :increment_visit_count
+  helper_method :visit_count
+
+  private
+
+  def initialize_session
+    # this will initialize the visit count to zero for new users.
+    # default value when the thing on the left hand side is UNSET, when nil set to 0
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    # this will increment the user's visit count in session.
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    # this will return the visit count from session.
+    session[:visit_count]
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # session[:visit_count] ||= 0 # default value when the thing on the left hand side is UNSET, when nil set to 0
+    # session[:visit_count] += 1
+    # @visit_count = session[:visit_count]
   end
 
   def show
