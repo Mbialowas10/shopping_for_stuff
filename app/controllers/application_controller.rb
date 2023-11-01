09ec3f96423e301142fe935e752b86388cabf421
mbialowas10 <mbialowas@rrc.ca>
2023-11-01 16:53:27 -0500

    s241185_added_session_to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  def initialize_session
+    # this will initialize the visit count to 0 for new users
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    # this will inrement the user's visit count in the seesion
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    session[:visit_count]
+  end
+
+  session[:visit_count] += 1
+  @visit_count = session[:visit_count]
 end
