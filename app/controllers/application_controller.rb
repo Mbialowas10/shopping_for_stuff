c6e8077a757eea680290c5a3f80f84e968e6ca60
mbialowas10 <mbialowas@rrc.ca>
2023-11-03 14:10:26 -0500

    s241185_session_complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session, :increment_visit_count
+  helper_method :visit_count
+
+  private
+
   def initialize_session
     # this will initialize the visit count to 0 for new users
     session[:visit_count] ||= 0
