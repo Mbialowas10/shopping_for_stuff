d10e1386e43ffa991a5bccf8fbf159b7288ed13c
mbialowas10 <mbialowas@rrc.ca>
2023-11-14 14:51:06 -0600

    checkout_Controller_stripe_241185

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,13 +3,39 @@ class CheckoutController < ApplicationController
     # Establish the connection with Stripe and redirect the user to the payment screen
     @product = Product.find(params[:product_id])
 
-    return unless @product.nil?
+    if @product.nil?
+      redirect_to root_path
+      return
+    end
 
-    redirect_to root_path
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url,
+      line_items: [
+        {
+          name: @product.name,
+          description: @product.description,
+          amount: @product.price_cents,
+          currency: 'cad',
+          quantity: 1
+        },
+        {
+          name: 'GST',
+          description: 'Goods and Services Tax',
+          amount: (@product.price_cents * 0.05).to_i,
+          currency: 'cad',
+          quantity: 1
+        }
+      ]
+    )
+    redirect_to @session.url, allow_other_host: true
   end
 
   def success
     # we took your money.
+    @session = Stripe::Checkout::Sesssion.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
   end
 
   def cancel
