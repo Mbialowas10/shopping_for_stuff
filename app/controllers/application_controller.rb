3af434c5d7cca12cc96c24cf41e553721ff94cbd
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-03-15 15:51:11 -0500

    Endpoint for March 15, 2024

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+  def initialize_session
+    session[:shopping_cart] ||= [] # represent an empty array of product ids
+  end
+
+  def cart
+    Product.find(session[:shopping_cart]) # pass an array of products ids  get a collection of products back
+  end
+
+
 end
